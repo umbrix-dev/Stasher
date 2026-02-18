5adb26c243df599081b7c548a23049887de90f22
umbrix-dev <JR34XT04@proton.me>
2026-02-18 20:39:52 +0100

    Created new main parser

---
+++ src/core/parser.py
@@ -0,0 +1,40 @@
+import argparse
+
+
+class Parser:
+    """The main argument parsing handler for Stasher."""
+
+    def __init__(self, parser: argparse.ArgumentParser) -> None:
+        self.subparsers = parser.add_subparsers(dest="command")
+        self.parsers = self._create_parsers()
+
+    def _create_parsers(self) -> dict[str, argparse.ArgumentParser]:
+        return {
+            "create": self._create_parser("create", "Create a new stash.", "name"),
+            "delete": self._create_parser("delete", "Delete a stash.", "name"),
+            "list": self._create_parser("list", "List all created stashes."),
+            "activate": self._create_parser("activate", "Activate a stash.", "name"),
+            "track": self._create_parser("track", "Track a path.", "path"),
+            "untrack": self._create_parser("untrack", "Untrack a path.", "path"),
+            "tracked": self._create_parser(
+                "tracked", "List all tracked paths in the current active stash."
+            ),
+            "auth": self._create_parser(
+                "auth", "Create a safety layer to securely handle dangerous actions."
+            ),
+            "wipe": self._create_parser("wipe", "Wipe all stashes."),
+            "status": self._create_parser("status", "Show current active stash."),
+            "update": self._create_parser(
+                "update", "Update current active stash with changed files."
+            ),
+            "merge": self._create_parser(
+                "merge", "Merge 2 stashes together.", "name_1", "name_2"
+            ),
+        }
+
+    def _create_parser(
+        self, name: str, help: str, *arguments: tuple[str]
+    ) -> argparse.ArgumentParser:
+        parser = self.subparsers.add_parser(name, help=help)
+        for argument in arguments:
+            parser.add_argument(argument)
